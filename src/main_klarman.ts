d7861f6428542f0fec91587ce27a3ef6d62c7b64
Klarman <klarmannrichard@gmail.com>
2023-05-26 17:39:51 +0100

    base 64 images for tv series

--- src/main_klarman.ts
+++ src/main_klarman.ts
@@ -4,8 +4,10 @@ import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import FastifyCors from '@fastify/cors';
 import fastifyJwt from '@fastify/jwt';
 import chalk from 'chalk';
-import { MOVIES, StreamingServers, TvType } from '@consumet/extensions';
+import { IMovieResult, MOVIES, StreamingServers, TvType } from '@consumet/extensions';
 import cache from './utils/cache';
+import axios from 'axios'
+// var base64 = require('node-base64-image');
 
 export const redis =
   process.env.REDIS_HOST &&
@@ -16,6 +18,45 @@ export const redis =
   });
 
 export const tmdbApi = process.env.apiKey && process.env.apiKey;
+
+// const convertImgToBase64 = (url: string) => {
+// 	var canvas = <HTMLCanvasElement | null> document.createElement('CANVAS');
+// 	var ctx = canvas!.getContext('2d');
+// 	var img = new Image;
+// 	img.crossOrigin = 'Anonymous';
+// 	img.onload = function() {
+// 		canvas!.height = img.height;
+// 		canvas!.width = img.width;
+//     ctx!.drawImage(img,0,0);
+//       var dataURL = canvas!.toDataURL('image/png')
+//       console.log(dataURL)
+// 	  	// callback.call(this, dataURL);
+//         // Clean up
+//     canvas = null;
+// 	};
+//   return url
+// 	// img.src = url;
+// }
+
+
+const getImage = async (url: string) => {
+  const image = await axios.get(url, {
+      responseType: 'arraybuffer'
+  });
+  const returnedB64 = Buffer.from(image.data).toString('base64');
+  // console.log(returnedB64)
+  return returnedB64
+}
+
+// async function urlToBase64(url) {
+//   return request.get(url, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       return "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+//     }
+//   });
+// }
+
+
 (async () => {
   const PORT = Number(process.env.PORT) || 3000;
 
@@ -56,24 +97,39 @@ export const tmdbApi = process.env.apiKey && process.env.apiKey;
     const flixhq = new MOVIES.FlixHQ();
 
     fastify.get('/main', async (_, rp) => {
-      const tvTrending = await cache.fetch(
-        redis as Redis,
-        `flixhq:trending:tv`,
-        async () => await flixhq.fetchTrendingTvShows(),
-        60 * 60 * 3
-      )
+      // const tvTrending = await cache.fetch(
+      //   redis as Redis,
+      //   `flixhq:trending:tv`,
+      //   async () => await flixhq.fetchTrendingTvShows(),
+      //   60 * 60 * 3
+      // )
 
-      const formattedTvTrending = tvTrending.map((item) => {
+      const tvTrending = await flixhq.fetchTrendingTvShows()
+
+      const formattedTvTrending = await Promise.all(tvTrending.map(async (item) => {
         return {
           id: item.id,
           title: item.title,
-          image: item.image,
+          image: await getImage(item.image!),
           latest_season: (item.season! as string).replace(/SS |S /g, ""),
           latest_episode: (item.latestEpisode! as string).replace(/EP |EPS /g, ""),
           type: "series",
           info_url: process.env.BASE_URL! + '/info?id=' + item.id
         }
-      })
+      }))
+
+
+      // const formattedTvTrending = tvTrending.map(async (item) => {
+      //   return {
+      //     id: item.id,
+      //     title: item.title,
+      //     image: item.image!,
+      //     latest_season: (item.season! as string).replace(/SS |S /g, ""),
+      //     latest_episode: (item.latestEpisode! as string).replace(/EP |EPS /g, ""),
+      //     type: "series",
+      //     info_url: process.env.BASE_URL! + '/info?id=' + item.id
+      //   }
+      // })
 
       const moviesTrending = await cache.fetch(
         redis as Redis,
@@ -211,6 +267,115 @@ export const tmdbApi = process.env.apiKey && process.env.apiKey;
           .send({ message: 'Something went wrong. Please try again later.' });
       }
     });
+
+    fastify.get('/search/:query', async (request: FastifyRequest, reply: FastifyReply) => {
+      const query = decodeURIComponent((request.params as { query: string }).query);
+
+      const page = (request.query as { page: number }).page;
+
+      let res = await cache.fetch(
+        redis as Redis,
+        `flixhq:${query}:${page}`,
+        async () => await flixhq.search(query, page ? page : 1),
+        60 * 60 * 6
+      )
+
+      const formattedResults = res.results.map((item) => {
+        if(item.type == TvType.MOVIE) {
+          return {
+            id: item.id,
+            title: item.title,
+            image: item.image,
+            release_date: item.releaseDate,
+            duration: item.duration,
+            type: "movie",
+            info_url: process.env.BASE_URL! + '/info?id=' + item.id
+          }
+        } else {
+          return {
+            id: item.id,
+            title: item.title,
+            image: item.image,
+            seasons: item.seasons ?? item.seasons,
+            type: "series",
+            info_url: process.env.BASE_URL! + '/info?id=' + item.id
+          }
+        }
+      })
+
+      const nextPageUrl = res.hasNextPage
+        ? process.env.BASE_URL! + '/search/' + encodeURIComponent(query) + '?page=' + (parseInt(res.currentPage!.toString()) + 1).toString() // dont ask
+        : null
+
+      const prevPageUrl = res.currentPage! > 1
+        ? process.env.BASE_URL! + '/search/' + encodeURIComponent(query) + '?page=' + (parseInt(res.currentPage!.toString()) - 1).toString() // dont ask
+        : null
+
+      reply.status(200).send({
+        next_page_url: nextPageUrl,
+        prev_page_url: prevPageUrl,
+        media_items: formattedResults
+      });
+    });
+
+    // fastify.get('/main', async (_, rp) => {
+    //   const tvTrending = await cache.fetch(
+    //     redis as Redis,
+    //     `flixhq:trending:tv`,
+    //     async () => await flixhq.fetchTrendingTvShows(),
+    //     60 * 60 * 3
+    //   )
+
+    //   const formattedTvTrending = tvTrending.map((item) => {
+    //     return {
+    //       id: item.id,
+    //       title: item.title,
+    //       image: item.image,
+    //       latest_season: (item.season! as string).replace(/SS |S /g, ""),
+    //       latest_episode: (item.latestEpisode! as string).replace(/EP |EPS /g, ""),
+    //       type: "series",
+    //       info_url: process.env.BASE_URL! + '/info?id=' + item.id
+    //     }
+    //   })
+
+    //   const moviesTrending = await cache.fetch(
+    //     redis as Redis,
+    //     `flixhq:trending:movies`,
+    //     async () => await flixhq.fetchTrendingMovies(),
+    //     60 * 60 * 3
+    //   )
+
+    //   const formattedMoviesTrending = moviesTrending.map((item) => {
+    //     return {
+    //       id: item.id,
+    //       title: item.title,
+    //       image: item.image,
+    //       release_date: item.releaseDate,
+    //       duration: item.duration,
+    //       type: "movie",
+    //       info_url: process.env.BASE_URL! + '/info?id=' + item.id
+    //     }
+    //   })
+
+    //   const results = {
+    //     sections: [
+    //       {
+    //         title: "Trending TV Series",
+    //         media_items: [
+    //           ...formattedTvTrending
+    //         ]
+    //       },
+    //       {
+    //         title: "Trending Movies",
+    //         media_items: [
+    //           ...formattedMoviesTrending
+    //         ]
+    //       }
+    //     ]
+    //   }
+
+    //   rp.status(200).send(results);
+    // });
 
     fastify.get('*', (request, reply) => {
       reply.status(404).send({
